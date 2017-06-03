b847080cceab9077fff0bfc6a88a40a9b75f79d5
Ilfirinwe <ilfirinwe@gmail.com>
2017-06-03 13:34:51 +1000

    Project initialization

---
+++ Kargers_RandomContraction/Kargers_RandomContraction/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kargers_RandomContraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
